757c4e94b5fd986230c3119d6a2ccf5df593f5c2
Ne0nX <d.karachentsov@gmail.com>
2014-06-22 14:08:26 +0300

    -

--- OfflineMaps Example/DKViewController.m
+++ OfflineMaps Example/DKViewController.m
@@ -17,19 +17,23 @@
     
     RMMBTilesSource *offlineSource = [[RMMBTilesSource alloc] initWithTileSetResource:@"HongKong" ofType:@"mbtiles"];
     
-    RMMapView *mapView = [[RMMapView alloc] initWithFrame:self.view.bounds andTilesource:offlineSource];
+    RMMapView *mapView = [[RMMapView alloc] initWithFrame:[self view].bounds andTilesource:offlineSource];
     
     [mapView setZoom:11];
     
     [mapView setMinZoom:11];
     [mapView setMaxZoom:14];
     
+    [mapView setCenterCoordinate:CLLocationCoordinate2DMake(22.327330,114.123419)];
+    
+    //Прячем лого MapBox и кнопку информации о карте. =)
     [mapView setShowLogoBug:NO];
     [mapView setHideAttribution:YES];
     
-    mapView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    [mapView setAutoresizingMask:(UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth)];
     
-    mapView.adjustTilesForRetinaDisplay = YES; // these tiles aren't designed specifically for retina, so make them legible
+    //Поддержка Retina Display
+    [mapView setAdjustTilesForRetinaDisplay:YES];
     
     [self.view addSubview:mapView];
 }
